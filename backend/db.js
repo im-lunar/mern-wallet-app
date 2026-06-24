747ce25062f0707b87f1875aec2782a4d267ad80
im-lunar <itslunarwrld@gmail.com>
2026-06-24 18:29:43 +0530

    Account schema created

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { number } = require("zod");
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URL);
@@ -32,8 +33,23 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
 const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Account', accountSchema);
 
 module.exports = {
-    User
+    User,
+    Account
 }
